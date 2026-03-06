b1675cbb23ffb4d77240868249a1e26e12330783
lawrenceokolo1 <lawrenceobabc@gmail.com>
2026-03-06 15:06:11 -0800

    add site

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProjectsSite from "../portfolio_site";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ProjectsSite />
+  </React.StrictMode>
+);
