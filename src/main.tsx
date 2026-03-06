3a6dd3f01118efa9c289f4556ba85b160c4d12d0
lawrenceokolo1 <lawrenceobabc@gmail.com>
2026-03-06 15:36:51 -0800

    Rename portfolio_site to ml_stack, update config and imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ProjectsSite from "../portfolio_site";
+import ProjectsSite from "../ml_stack";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
